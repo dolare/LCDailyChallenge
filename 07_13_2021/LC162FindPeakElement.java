414096f012d3facd7574b4d995d5efa639be244f
dolare <dolarebrave@gmail.com>
2021-07-14 02:23:59 -0400

    Create LC162FindPeakElement.java

---
+++ 07_13_2021/LC162FindPeakElement.java
@@ -0,0 +1,19 @@
+class LC162FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        // use binray search to get the first/last element that less than its next element
+        // eg: 1 5 3 4 7 5 -> get 7
+        // eg: 1 5 3 4 7 9 -> get 9
+        if (nums.length == 1) return 0;
+        int left = 0, right = nums.length - 1;
+        
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] > nums[right] ? left : right;
+    }
+}
