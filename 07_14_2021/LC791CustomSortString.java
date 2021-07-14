568561636d3df8ee54d2e2aff5f557cddd5d3c36
dolare <dolarebrave@gmail.com>
2021-07-14 15:33:47 -0400

    Update LC791CustomSortString.java

--- 07_14_2021/LC791CustomSortString.java
+++ 07_14_2021/LC791CustomSortString.java
@@ -1,38 +1,25 @@
 class LC791CustomSortString {
     public String customSortString(String order, String str) {
-        // use charArray to check if letter exist and the count of each letter
+        // use charArray to store the count
         int[] charCount = new int[26];
-        Arrays.fill(charCount, -1);
-        // use set to store the index that has the letter we want to reorder
-        Set<Integer> charIndex = new HashSet();
-        for (char c: order.toCharArray()) {
-            charCount[c - 'a']++;
-        }
-        // store index for all the letters in the order
-        for (int i = 0; i < str.length(); i++) {
-            if (charCount[str.charAt(i) - 'a'] != -1) {
-                charIndex.add(i);
-                charCount[str.charAt(i) - 'a']++;
-            }
-        }
-        
-        // reorder
+
+        // count letters
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            if (charIndex.contains(i)) {
-                for (char c: order.toCharArray()) {
-                    if (charCount[c - 'a'] > 0) {
-                        sb.append(c);
-                        charCount[c - 'a']--;
-                        break;
-                    }
-                }
+            if(!order.contains(str.substring(i, i + 1))) {
+                sb.append(str.charAt(i));   
             } else {
-                sb.append(str.charAt(i));
+                 charCount[str.charAt(i) - 'a']++;
             }
         }
         
-        return sb.toString();
+        // add reordered letters
+        for(char c: order.toCharArray()) {
+           while(charCount[c - 'a']-- > 0) {
+               sb.append(c);
+           }
+        }
         
+        return sb.toString();    
     }
 }
