36d8e68d66167db109334a668ccbdede67e5a268
dolare <dolarebrave@gmail.com>
2021-07-26 15:10:42 -0400

    Update LC108ConvertSortedArrayToBinarySearchTree.java

--- 07_26_2021/LC108ConvertSortedArrayToBinarySearchTree.java
+++ 07_26_2021/LC108ConvertSortedArrayToBinarySearchTree.java
@@ -8,7 +8,7 @@ public class LC108ConvertSortedArrayToBinarySearchTree {
 
     public TreeNode constructTree(int[] nums, int left, int right, TreeNode root) {
         if (left > right) return null;;
-        if (left == right) return new TreeNode(nums[left]);
+        // if (left == right) return new TreeNode(nums[left]);
         int mid = left + (right - left)/2;
         root.val = nums[mid];
         root.left = constructTree(nums, left, mid - 1, new TreeNode());
