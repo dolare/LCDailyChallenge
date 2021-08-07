f2882e0bf935b94a1be05fa0f5aa299ff1bd2a05
dolare <dolarebrave@gmail.com>
2021-08-07 13:14:10 -0400

    0807

---
+++ 08_07_2021/LC132PalindromePartitioningII.java
@@ -0,0 +1,39 @@
+public class LC132PalindromePartitioningII {
+    public int minCut(String s) {
+
+        // DP solution:
+        // dp[i] -> the mincut for the substring(0, i)
+        // dp[i] = Math.min(dp[0]....d[i]) + 1 only when substring(i, n - 1) is palindrom
+
+        int n = s.length();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = i;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (isPalindrom(s, j, i)) {
+                    if (j == 0) {
+                        // the whole substring(0, i) is palindrom
+                        dp[i] = 0;
+                    } else {
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+
+    // can use dp[i][j] to maintin isPalindrom for substring(i, j),
+    // TC: n^2 SC: n^2 to construct the two dimentional array
+    // TC: 1 SC: 1 to get isPalindrom after creating the array above
+    public boolean isPalindrom(String s, int left, int right) {
+        while(left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
